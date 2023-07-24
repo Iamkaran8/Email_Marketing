d0855be8790910dcff9bbafa3989324506d6c804
karanwebdev <karanwebdev8@gmail.com>
2023-07-24 10:15:06 +0530

    new project

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// $(document).ready(function () {
+//     $('#hoverBox')
+//     .bind('mouseover',function(mouseover) {
+//         var val = $('span').val()
+//         $("span").hide();
+//         $('#hoverBox').text("hai")
+//     })
+//     .bind('mouseleave',function(mouseover) {
+//         $("span").show(val);
+//     });
+
+// });
